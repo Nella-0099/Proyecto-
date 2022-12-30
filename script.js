0db85bed616801b3e08f4f7c74552da617a0bb02
Nella-0099 <brunealor0099@gmail.com>
2022-12-29 19:28:50 -0500

    mi primer commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function encriptar(){
+    const passSistema = "1234";
+    const msgError = "Contraseña Incorrecta";
+    var mensaje = document.getElementById("msg").value;
+    var password = document.getElementById("pass").value;
+    if(passSistema==password){
+        document.getElementById("error").innerHTML = "";
+        var encriptado = btoa(mensaje);
+        document.getElementById("demo1").innerHTML = encriptado;
+        document.getElementById("demo2").innerHTML = "";
+        document.getElementById("msgCodigo").value = "";
+    }else{
+        document.getElementById("error").innerHTML = msgError;
+        document.getElementById("demo1").innerHTML = "";
+        document.getElementById("msgCodigo").value = "";
+        document.getElementById("demo2").innerHTML = "";
+    }
+}
+
+function desencriptar(){
+    var mensajeCodigo = document.getElementById("msgCodigo").value;
+    var decrypted = atob(mensajeCodigo);
+    document.getElementById("demo2").innerHTML = decrypted;
+}
